d8bfb7a1a611ef4a261e2bf628bf298633ca3de9
Joshua Hoffman <hoffmanjoshua@outlook.com>
2018-08-03 09:19:38 -0700

    fixed sort on admin page

--- routes/index.js
+++ routes/index.js
@@ -105,6 +105,17 @@ app.get('/admin', function (req, res) {
           res.redirect('/')
         } else if (result) {
           console.log(result[0].name);
+          result.sort(function (a, b) {
+            var x = a.name.toLowerCase();
+            var y = b.name.toLowerCase();
+            if (x < y) {
+              return -1;
+            }
+            if (x > y) {
+              return 1;
+            }
+            return 0;
+          });
           res.render('admincontrols', {
             schools: result
           });
